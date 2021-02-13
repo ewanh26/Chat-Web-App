cb567d05a0a8c97142864ae9d24f313727306f96
ewanh26 <73652324+ewanh26@users.noreply.github.com>
2021-02-13 21:58:13 +0000

    Second Slide moon; scroll animation nearly there

--- js/index.js
+++ js/index.js
@@ -5,28 +5,40 @@ const $prevButton = document.getElementById('prev')
 const $nextButton = document.getElementById('next');
 const $div = document.getElementById('maindiv')
 
+function update() {    
+    switch (pageCounter) {
+        default:
+            $div.className = 'slide1Header';
+            $div.innerHTML = '<h1>THE WORLD OF TOMORROW. WHAT\'S NEXT FOR US?</h1>'
+            break;
+        case 1:
+            $div.className = 'slide2';
+            $div.innerHTML = `
+            <img src="Images/starry.jpg" alt="stars" id="starimg">
+            <img src="Images/moon.png" alt="moon" id="moonimg">
+            `;
+            break;
+    }
+}
+
+update();
+
 $prevButton.addEventListener('click', function () {
     if (pageCounter > 0) {
         pageCounter--;
         console.log(pageCounter);
+        update();
     }
 });
 $nextButton.addEventListener('click', function () {
     if (pageCounter < maxPages) {
         pageCounter++;
         console.log(pageCounter);
+        update();
     }
 });
 
-setInterval(function () {
-    switch (pageCounter) {
-        case 0:
-            $div.className = 'slide1Header';
-            $div.innerHTML = '<h1>THE WORLD OF TOMORROW. WHAT\'S NEXT FOR US?</h1>'
-            break;
-        case 1:
-            $div.className = 'slide2';
-            $div.innerHTML = '<img src="Images/starry.jpg" alt="stars" id="starimg">';
-            break;
-    }
-}, 1);
+window.addEventListener('scroll', function () {
+    document.body.style.setProperty('--scroll', window.pageYOffset / (document.body.offsetHeight - window.innerHeight));
+});
+
